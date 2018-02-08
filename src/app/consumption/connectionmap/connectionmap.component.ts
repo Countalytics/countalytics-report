1ba215e7f32285bf9cbbc1904c5db82e853545f1
tessav <tessavoon@gmail.com>
2018-02-09 01:14:15 +0800

    fix horizontal scroll

--- src/app/consumption/connectionmap/connectionmap.component.ts
+++ src/app/consumption/connectionmap/connectionmap.component.ts
@@ -9,6 +9,7 @@ declare var Plotly: any;
   styleUrls: ['./connectionmap.component.scss']
 })
 export class ConnectionmapComponent implements OnInit {
+  flightPairData;
 
   constructor(private cservice: ConsumptionService) { }
 
@@ -28,73 +29,83 @@ export class ConnectionmapComponent implements OnInit {
           flight["end_lon"] = airportDict[flight["Arvl"]]["Longitude"];
           flight["cnt"] = 1;
         }
+        // find flight pairs set, get average consumption (sum(%) / data points)
 
-        let data = [];
-        let count = this.unpack(consumptionData, 'cnt');
-        let startLongitude = this.unpack(consumptionData, 'start_lon');
-        let endLongitude = this.unpack(consumptionData, 'end_lon');
-        let startLat = this.unpack(consumptionData, 'start_lat');
-        let endLat = this.unpack(consumptionData, 'end_lat');
-        let origin = this.unpack(consumptionData, 'Dept');
-        let destination = this.unpack(consumptionData, 'Arvl');
-
-        for ( let i = 0 ; i < count.length; i++ ) {
-          let opacityValue = consumptionData[i]/this.getMaxOfArray(consumptionData);
-          let result2 = {
-            type: 'scattergeo',
-            //locationmode: "ISO-3",
-            lon: [-startLongitude[i], -endLongitude[i]],
-            lat: [startLat[i] , endLat[i]],
-            mode: 'lines+text',
-            text: [origin[i], destination[i]],
-            line: {
-              width: 2,
-              color: 'red',
-              dash: 'dot'
-            }
-            //opacity: 1//opacityValue
-          };
-          data.push(result2);
-        }
-
-        console.log(data);
-
-        let layout = {
-          showlegend: false,
-          geo:{
-            scope: 'usa',
-            projection: {
-              type: 'albers usa'
-            },
-            showland: true,
-            landcolor: "rgb(250, 250, 250)",
-            subunitcolor: "rgb(217, 217, 217)",
-            countrycolor: 'rgb(217, 217, 217)'
-          },
-          height: 400,
-          width: 600,
-          margin: {
-            l: 0,
-            r: 0,
-            b: 0,
-            t: 0,
-            pad: 0
-          },
-          yaxis: {autorange: true},
-          xaxis : {autorange: true}
-        };
-        let ctx = document.getElementById("myDiv");
-        Plotly.plot(ctx, data, layout, {showLink: false, displayModeBar: false, scrollZoom: false});
+        this.renderMap(consumptionData);
       });
     });
   }
 
-  unpack(rows, key) {
-    return rows.map(function(row) { return row[key]; });
+  renderMap(consumptionData) {
+    let data = [];
+    let count = this.unpack(consumptionData, 'cnt');
+    let startLongitude = this.unpack(consumptionData, 'start_lon');
+    let endLongitude = this.unpack(consumptionData, 'end_lon');
+    let startLat = this.unpack(consumptionData, 'start_lat');
+    let endLat = this.unpack(consumptionData, 'end_lat');
+    let origin = this.unpack(consumptionData, 'Dept');
+    let destination = this.unpack(consumptionData, 'Arvl');
+    let consumption = this.unpack(consumptionData, 'Consumption');
+
+    for ( let i = 0 ; i < count.length; i++ ) {
+      let opacityValue = consumptionData[i]/this.getMaxOfArray(consumptionData);
+      let color = (consumption[i] > 75) ? '#E71D36' : '#32DE8A'; // set threshold for low and high consumption
+      let result2 = {
+      type: 'scattergeo',
+      //locationmode: "ISO-3",
+      lon: [-startLongitude[i], -endLongitude[i]],
+      lat: [startLat[i] , endLat[i]],
+      mode: 'lines+text',
+      text: [origin[i], destination[i]],
+      line: {
+        width: 2,
+        color: color,
+        dash: 'dot'
+      },
+      name: `${consumptionData[i]["Flt #"]} : ${consumptionData[i]["Dept"]} -> ${consumptionData[i]["Arvl"]}`
+      //opacity: 1//opacityValue
+    }
+    data.push(result2);
   }
 
-  getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-  }
+  let layout = {
+    showlegend: true,
+    legend: {
+
+    },
+    geo:{
+      scope: 'usa',
+      projection: {
+        type: 'albers usa'
+      },
+      showland: true,
+      landcolor: "rgb(250, 250, 250)",
+      subunitcolor: "rgb(217, 217, 217)",
+      countrycolor: 'rgb(217, 217, 217)'
+    },
+    height: 400,
+    width: 900,
+    margin: {
+      l: 0,
+      r: 0,
+      b: 0,
+      t: 0,
+      pad: 0
+    },
+    yaxis: {autorange: true},
+    xaxis : {autorange: true}
+  };
+  let ctx = document.getElementById("myDiv");
+  Plotly.plot(ctx, data, layout, {showLink: false, displayModeBar: false, scrollZoom: false});
+
+}
+
+unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
 
 }
